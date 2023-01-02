5dbd8e9748bf86ac733a2efe9935d7f179625146
Marija Havaic <marija.havaic@gmail.com>
2023-01-02 16:27:27 +0100

    Add content and style it

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return <div className="App">
+    <div className="quote-container">
+      <small>Advice #123</small>
+      <p>Quote</p>
+    </div>
+    </div>;
+}
+
+export default App;
