5d3144f084e4cd1808d142b3c0eab270d089aec5
Marija Havaic <marija.havaic@gmail.com>
2023-01-02 17:04:12 +0100

    Add react-device-detect lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.scss";
+import {isMobile} from 'react-device-detect';
+
+import desktop_divider from './images/pattern-divider-desktop.svg'
 
 function App() {
-  return <div className="App">
-    <div className="quote-container">
-      <small>Advice #123</small>
-      <p>Quote</p>
+  {/* Check the device */}
+  {/* fetch API */}
+  return(
+  <div className="App">
+      <div className="quote-container">
+        <small>Advice #123</small>
+        <p>Quote</p>
+        <img src={desktop_divider} alt="Pattern divider" />
     </div>
-    </div>;
+  </div>)
 }
 
 export default App;
