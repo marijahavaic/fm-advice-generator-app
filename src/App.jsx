8c8e6ac0db6b64b819a3986ef85c1f51bcfa921e
Marija Havaic <marija.havaic@gmail.com>
2023-01-03 10:59:48 +0100

    Add a button with a dice

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import {isMobile} from 'react-device-detect';
 
 import desktop_divider from './images/pattern-divider-desktop.svg'
 import mobile_divider from './images/pattern-divider-mobile.svg'
+import icon_dice from './images/icon-dice.svg'
 
 function App() {
-  {/* Check the device */}
-  {/* fetch API */}
+  console.log(isMobile)
   return(
   <div className="App">
       <div className="quote-container">
         <small>Advice #123</small>
         <p>Quote</p>
-        <img src={isMobile ? mobile_divider : desktop_divider} alt="Pattern divider" />
+        <img src={isMobile ? mobile_divider : desktop_divider} alt="Pattern divider" className="divider" />
+        <button className="dice">
+          <img src={icon_dice} alt="Icon dice" />
+        </button>
     </div>
   </div>)
 }
